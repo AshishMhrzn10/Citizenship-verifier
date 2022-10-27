d8d6d18e3ac3033a68fd5310e01ff90a00684192
AshishMhrzn10 <amaharjan1032@gmail.com>
2022-10-27 23:00:45 +0545

    Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,13 @@ import Register from './components/Register/Register';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Status from './components/Status/Status';
 import Post from './components/Post/Post';
+import image from "./images/image.jpg";
 
 function App() {
   const loginUser = localStorage.getItem('users');
   return (
     <div className="App">
+      <img src={image} alt="" style={{ width: "100%" }} />
       <Router>
         <Routes>
           {
