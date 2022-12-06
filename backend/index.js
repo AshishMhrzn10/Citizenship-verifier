89767559a3ca2cc4f8198f3bb05fe6ec307eb1fd
Ashish Maharjan <ashish@explorug.net>
2022-12-06 20:41:02 +0545

    Final commit

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
 const app = express();
 dotenv.config();
 mongoose.connect(process.env.MONGO_URI, {
@@ -49,7 +54,7 @@ const UserInfo = new mongoose.model("UserInfo", userInfoSchema);
 
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 //Routes
 app.post("/login", async (req, res) => {
     const { email, password } = req.body;
--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,81 +1,97 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import "./home.css";
 import { Link, useNavigate } from "react-router-dom";
-import { AppBar, Toolbar, Typography, Button, Box, Card, CardMedia, CardContent, CardActions, Grid } from "@material-ui/core";
-import Form from '../Form/Form';
-import axios from 'axios';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Box,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Grid,
+} from "@material-ui/core";
+import Form from "../Form/Form";
+import axios from "axios";
 
 const Home = () => {
-    const [allPost, setAllPost] = useState();
-    const navigate = useNavigate();
-    const user = JSON.parse(localStorage.getItem("users"));
-    const remove = () => {
-        localStorage.removeItem('users');
-        navigate("/login");
-    };
-    useEffect(() => {
-        if (user.status === "pending" || user.status === "verified") {
-            navigate("/status");
-        }
+  const [allPost, setAllPost] = useState();
+  const navigate = useNavigate();
+  const user = JSON.parse(localStorage.getItem("users"));
+  const remove = () => {
+    localStorage.removeItem("users");
+    navigate("/login");
+  };
+  useEffect(() => {
+    if (user?.status === "pending" || user?.status === "verified") {
+      navigate("/status");
+    }
 
-        axios.get("http://localhost:8800/posts")
-            .then(res => {
-                setAllPost(res.data);
-            })
-            .catch(err => console.log(err));
-    }, []);
+    axios
+      .get("http://localhost:8800/posts")
+      .then((res) => {
+        setAllPost(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    return (
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              KYC Verifier
+            </Typography>
+            <Box sx={{ flexGrow: 1 }} />
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Hello, {user?.name}
+            </Typography>
+            <Box sx={{ flexGrow: 1 }} />
+            <Button color="inherit" onClick={remove}>
+              Logout
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      {user?.isAdmin ? (
+        <Grid container spacing={2} style={{ marginTop: "2rem" }}>
+          {allPost?.map((item) => (
+            <Grid item xs={4} key={item._id}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                  component="img"
+                  height="240"
+                  image={item.image}
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {item.name}
+                  </Typography>
+                  <Typography variant="body2">{item.email}</Typography>
+                </CardContent>
+                <CardActions>
+                  <Link to={`post/${item._id}/${item.email}`}>
+                    Check for details
+                  </Link>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      ) : (
         <>
-            <Box sx={{ flexGrow: 1 }}>
-                <AppBar position="static">
-                    <Toolbar>
-                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                            KYC Verifier
-                        </Typography>
-                        <Box sx={{ flexGrow: 1 }} />
-                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                            Hello, {user.name}
-                        </Typography>
-                        <Box sx={{ flexGrow: 1 }} />
-                        <Button color="inherit" onClick={remove}>Logout</Button>
-                    </Toolbar>
-                </AppBar>
-            </Box>
-            {user.isAdmin ? (
-                <Grid container spacing={2} style={{ marginTop: "2rem" }}>
-                    {allPost?.map((item) => (
-                        <Grid item xs={4} key={item._id}>
-                            <Card sx={{ maxWidth: 345 }}>
-                                <CardMedia
-                                    component="img"
-                                    height="240"
-                                    image={item.image}
-                                    alt="green iguana"
-                                />
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div">
-                                        {item.name}
-                                    </Typography>
-                                    <Typography variant="body2" >
-                                        {item.email}
-                                    </Typography>
-                                </CardContent>
-                                <CardActions>
-                                    <Link to={`post/${item._id}/${item.email}`}>Check for details</Link>
-                                </CardActions>
-                            </Card>
-                        </Grid>
-                    ))}
-                </Grid>
-            ) : (
-                <>
-                    <Typography variant="h4" style={{ marginTop: "2rem" }}>Submit your Details below:</Typography>
-                    <Form />
-                </>
-            )}
+          <Typography variant="h4" style={{ marginTop: "2rem" }}>
+            Submit your Details below:
+          </Typography>
+          <Form />
         </>
-    );
+      )}
+    </>
+  );
 };
 
 export default Home;
